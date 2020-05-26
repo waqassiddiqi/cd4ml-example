7323262b8c650a33788941347cbcc93b672baebe
Waqas Hussain Siddiqui <waqas.hussain@outlook.com.au>
2020-05-26 18:55:00 +0800

    debugging

--- code/train/train.py
+++ code/train/train.py
@@ -1,130 +1,101 @@
 import os
-import argparse
-import itertools
-import numpy as np
+import warnings
+import sys
+
+from dotenv import load_dotenv
+
 import pandas as pd
-import joblib
-import matplotlib.pyplot as plt
-
-from sklearn import datasets
-from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 
-from azureml.core import Dataset, Run
-run = Run.get_context()
+from azureml.core import Run, Workspace, Dataset
+from azureml.core.authentication import ServicePrincipalAuthentication
 
-
-def log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix', title='Confusion matrix', cmap=plt.cm.Blues):
-    '''
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    '''
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
-        print('Normalized confusion matrix')
-    else:
-        print('Confusion matrix, without normalization')
-    print(cm)
-    
-    plt.figure()
-    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels, rotation = 45)
-    plt.yticks(tick_marks, labels)
-    
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment = "center", color = 'white' if cm[i, j] > thresh else 'black')
-    
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-    run.log_image(log_name, plot=plt)
-    plt.savefig(os.path.join('outputs', '{0}.png'.format(log_name)))
+import logging
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
 
-def log_confusion_matrix(cm, labels):
-    # log confusion matrix as object
-    cm_json =   {
-       'schema_type': 'confusion_matrix',
-       'schema_version': 'v1',
-       'data': {
-           'class_labels': labels,
-           'matrix': cm.tolist()
-       }
-    }
-    run.log_confusion_matrix('confusion_matrix', cm_json)
-    
-    # log confusion matrix as image
-    log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix_unnormalized', title='Confusion matrix')
-    
-    # log normalized confusion matrix as image
-    log_confusion_matrix_image(cm, labels, normalize=True, log_name='confusion_matrix_normalized', title='Normalized confusion matrix')
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 
 
 def main(args):
-    # create the outputs folder
-    os.makedirs('outputs', exist_ok=True)
-    
-    # Log arguments
-    run.log('Kernel type', np.str(args.kernel))
-    run.log('Penalty', np.float(args.penalty))
+    load_dotenv()
+    train_on_local = os.environ.get("TRAIN_LOCAL") is not None and os.environ["TRAIN_LOCAL"] == "True"
 
-    # Load iris dataset
-    X, y = datasets.load_iris(return_X_y=True)
-    
-    #dividing X,y into train and test data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=223)
-    data = {'train': {'X': x_train, 'y': y_train},
-            'test': {'X': x_test, 'y': y_test}}
-    
-    # train a SVM classifier
-    svm_model = SVC(kernel=args.kernel, C=args.penalty, gamma='scale').fit(data['train']['X'], data['train']['y'])
-    svm_predictions = svm_model.predict(data['test']['X'])
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-    # accuracy for X_test
-    accuracy = svm_model.score(data['test']['X'], data['test']['y'])
-    print('Accuracy of SVM classifier on test set: {:.2f}'.format(accuracy))
-    run.log('Accuracy', np.float(accuracy))
-    
-    # precision for X_test
-    precision = precision_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('Precision of SVM classifier on test set: {:.2f}'.format(precision))
-    run.log('precision', precision)
-    
-    # recall for X_test
-    recall = recall_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('Recall of SVM classifier on test set: {:.2f}'.format(recall))
-    run.log('recall', recall)
-    
-    # f1-score for X_test
-    f1 = f1_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('F1-Score of SVM classifier on test set: {:.2f}'.format(f1))
-    run.log('f1-score', f1)
-    
-    # create a confusion matrix
-    labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-    labels_numbers = [0, 1, 2]
-    cm = confusion_matrix(y_test, svm_predictions, labels_numbers)
-    log_confusion_matrix(cm, labels)
-    
-    # files saved in the "outputs" folder are automatically uploaded into run history
-    model_file_name = "model.pkl"
-    joblib.dump(svm_model, os.path.join('outputs', model_file_name))
+    if(train_on_local == True):
+        ws = Workspace.get(os.environ["AZURE_ML_WORKSPACE_NAME"], 
+        subscription_id=os.environ["AZURE_ML_SUBSCRIPTION_ID"], 
+        resource_group=os.environ["AZURE_RESOURCE_GROUP"])
 
+    else:
+        run = Run.get_context()
+        print(run)
+        ws = run.experiment.workspace
+        print(ws)
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--kernel', type=str, default='rbf', help='Kernel type to be used in the algorithm')
-    parser.add_argument('--penalty', type=float, default=1.0, help='Penalty parameter of the error term')
-    args = parser.parse_args()
-    return args
+    uri = ws.get_mlflow_tracking_uri()
+    mlflow.set_tracking_uri(uri)
+
+    # Get data from Azure ML workspace Dataset
+    try:
+        data = Dataset.get_by_name(workspace=ws, name="WineQualityRedDS").to_pandas_dataframe()
+    except Exception as e:
+        logger.exception(
+            "WineQualityRedDS data set not found please check Datasets in Azure ML workspace. Error: %s", e)
+    data.fillna(data.mean(), inplace=True)
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        # log parameters
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+
+        # log metrics
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        # log model
+        mlflow.sklearn.log_model(lr, "model")
+
+    run_metrics = run.get_metrics(recursive=True)
+    print(run_metrics)
 
 
 if __name__ == '__main__':
-    args = parse_args()
-    main(args=args)
+    main(args=[])
